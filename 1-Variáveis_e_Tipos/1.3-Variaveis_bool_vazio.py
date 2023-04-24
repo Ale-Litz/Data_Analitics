1fd813e651503e186a9fbcbbed4ff7de4460ee99
Alexandre LITZ <77252253+Ale-Litz@users.noreply.github.com>
2023-04-23 22:43:01 -0300

    Create 1.3-Variaveis_bool_vazio.py

---
+++ 1-Variáveis_e_Tipos/1.3-Variaveis_bool_vazio.py
@@ -0,0 +1,12 @@
+# BOOL
+usuario_maior_de_idade = True
+
+print(usuario_maior_de_idade)
+print(type(usuario_maior_de_idade))
+
+# VAZIO
+
+telefone_fixo = None
+
+print(telefone_fixo)
+print(type(telefone_fixo))
